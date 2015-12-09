d4fe4ceb1d76a3e4d2ef5915019a11109adfb325
Thanos Korakas <thanoskorakas@gmail.com>
2015-12-09 23:48:27 +0200

    Working on gulpfile.

--- app/index.js
+++ app/index.js
@@ -7,117 +7,120 @@ var chalk = require('chalk');
 
 module.exports = generators.Base.extend({
 
-	constructor: function(){
-		generators.Base.apply(this, arguments);
-		this.option('coffee');
-		this.option('less');
-		this.option('sass');
-		this.jsExtension = this.options.coffee ? "coffee" : "js";
-		if(!this.options.sass && !this.options.less){
-			this.cssExtension = "css"
-		}else{
-			this.cssExtension = this.options.sass ? "scss" : "less"
-		}
-	},
-	_createFileStructrure: function(){
-		var destRoot = this.destinationRoot();
-		var appDir = destRoot + '/app';
-		var templateContext = {
-                appName: this.appName,
-                appDescription: this.appDescription,
-                appVersion: this.appVersion,
-                appAuthor: this.appAuthor,
-                appEmail: this.appEmail,
-                appLicense: this.appLicense,
-                appGit: this.appGit,
-                appWebsite: this.appWebsite,
-				css: this.cssExtension,
-            };
-		mkdirp(appDir + '/js');
-		mkdirp(appDir + '/js/controllers');
-		mkdirp(appDir + '/js/services');
-		mkdirp(appDir + '/js/directives');
-		mkdirp(appDir + '/js/filters');
-		mkdirp(appDir + '/style');
-		mkdirp(appDir + '/images');
-		mkdirp(appDir + '/tests');
-		if(this.cssExtension != 'less' && this.cssExtension != 'sass'){
-			mkdirp(appDir + '/style/' + this.cssExtension);
-		}
-		mkdirp(appDir + '/templates');
+    constructor: function () {
+        generators.Base.apply(this, arguments);
+        this.option('coffee');
+        this.option('less');
+        this.option('sass');
+        this.jsExtension = this.options.coffee ? "coffee" : "js";
+        if (!this.options.sass && !this.options.less) {
+            this.cssExtension = "css"
+        } else {
+            this.cssExtension = this.options.sass ? "scss" : "less"
+        }
+    },
+    _createFileStructrure: function () {
+        var destRoot = this.destinationRoot();
+        var appDir = destRoot + '/app';
+        var templateContext = {
+            appName: this.appName,
+            appDescription: this.appDescription,
+            appVersion: this.appVersion,
+            appAuthor: this.appAuthor,
+            appEmail: this.appEmail,
+            appLicense: this.appLicense,
+            appGit: this.appGit,
+            appWebsite: this.appWebsite,
+            css: this.cssExtension,
+        };
 
-		this.fs.copy(this.sourceRoot() + '/root/_.bowerrc', destRoot + '/.bowerrc');
-		this.fs.copy(this.sourceRoot() + '/root/_bower.json', destRoot + '/bower.json');
-		this.fs.copyTpl(this.sourceRoot() + '/root/_index.html', destRoot + '/index.html', templateContext);
-		this.fs.copyTpl(this.sourceRoot() + '/root/_app.' + this.jsExtension, appDir + '/app.' +  this.jsExtension, templateContext);
-		this.fs.copy(this.sourceRoot() + '/javascript/' + this.jsExtension + '/mainCtrl.' + this.jsExtension, appDir + '/js/controllers/mainCtrl.' +  this.jsExtension);
-		 if(this.option.less && this.option.sass){
-		 	this.fs.copy(this.sourceRoot() + '/style/style.' + this.cssExtension, appDir + '/style/' + this.cssExtension + 'style.' + this.cssExtension);
-		 }else{
-		 	this.fs.copy(this.sourceRoot() + '/style/style.' + this.cssExtension, appDir + '/style/style.' + this.cssExtension);
-		 }
-	},
-	initializing: function(){
-		var message = chalk.blue('Welcome to Angularize ' + chalk.red('A simple AngularJS generator.'));
-		this.log(yosay(message, {maxLength: 21}));
-	},
-	prompting: function(){
-		var done = this.async();
-	    this.prompt([{
-	      type    : 'input',
-	      name    : 'name',
-	      message : 'Your project name:',
-	      default : this.appname,
-	    }
-	    ,{
-	    	type: 'input',
-	    	name: 'description',
-	    	message: 'Project description:'
-	    }
-	    ,{
-	    	type: 'input',
-	    	name: 'author',
-	    	message: 'Your name:'
-	    }
-	    ,{
-	    	type: 'input',
-	    	name: 'email',
-	    	message: 'Your email:'
-	    }
-	    ,{
-	    	type: 'input',
-	    	name: 'git',
-	    	message: 'Your project git repository:'
-	    }
-	    ,{
-	    	type: 'input',
-	    	name: 'version',
-	    	message: 'Project version:',
-	    	default: '0.0.0'
-	    }
-	    ,{
-	    	type: 'input',
-	    	name: 'license',
-	    	message: 'Project license type:',
-	    	default: 'MIT'
-	    }
-	    ,{
-	    	type: 'input',
-	    	name: 'website',
-	    	message: 'Your website:'
-	    }], function (answers) {
-	      this.appName = answers.name;
-	      this.appDescription = answers.description;
-	      this.appAuthor = answers.author;
-	      this.appEmail = answers.email;
-	      this.appRepo = answers.git;
-	      this.appVersion = answers.version;
-	      this.appLicense = answers.license;
-	      this.appWebsite = answers.website;
-	      done();
-	    }.bind(this));
-	},
-	writing: function(){
-		this._createFileStructrure();
-	}
+        // Create folder structure.
+        mkdirp(appDir + '/js');
+        mkdirp(appDir + '/js/controllers');
+        mkdirp(appDir + '/js/services');
+        mkdirp(appDir + '/js/directives');
+        mkdirp(appDir + '/js/filters');
+        mkdirp(appDir + '/style');
+        mkdirp(appDir + '/images');
+        mkdirp(appDir + '/tests');
+        mkdirp(appDir + '/templates');
+
+        if (this.cssExtension != 'less' && this.cssExtension != 'sass') {
+            mkdirp(appDir + '/style/' + this.cssExtension);
+        }
+
+        this.fs.copy(this.sourceRoot() + '/root/_.bowerrc', destRoot + '/.bowerrc');
+        this.fs.copy(this.sourceRoot() + '/root/_bower.json', destRoot + '/bower.json');
+        this.fs.copyTpl(this.sourceRoot() + '/root/_index.html', appDir + '/index.html', templateContext);
+        this.fs.copyTpl(this.sourceRoot() + '/root/_app.' + this.jsExtension, appDir + '/app.' + this.jsExtension, templateContext);
+        this.fs.copy(this.sourceRoot() + '/javascript/' + this.jsExtension + '/mainCtrl.' + this.jsExtension, appDir + '/js/controllers/mainCtrl.' + this.jsExtension);
+        if (this.option.less && this.option.sass) {
+            this.fs.copy(this.sourceRoot() + '/styles/style.' + this.cssExtension, appDir + '/styles/' + this.cssExtension + 'style.' + this.cssExtension);
+        } else {
+            this.fs.copy(this.sourceRoot() + '/styles/style.' + this.cssExtension, appDir + '/styles/style.' + this.cssExtension);
+        }
+    },
+    initializing: function () {
+        var message = chalk.blue('Welcome to Angularize ' + chalk.red('A simple AngularJS generator.'));
+        this.log(yosay(message, {maxLength: 21}));
+    },
+    prompting: function () {
+        var done = this.async();
+        this.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'Your project name:',
+            default: this.appname,
+        }
+            , {
+                type: 'input',
+                name: 'description',
+                message: 'Project description:'
+            }
+            , {
+                type: 'input',
+                name: 'author',
+                message: 'Your name:'
+            }
+            , {
+                type: 'input',
+                name: 'email',
+                message: 'Your email:'
+            }
+            , {
+                type: 'input',
+                name: 'git',
+                message: 'Your project git repository:'
+            }
+            , {
+                type: 'input',
+                name: 'version',
+                message: 'Project version:',
+                default: '0.0.0'
+            }
+            , {
+                type: 'input',
+                name: 'license',
+                message: 'Project license type:',
+                default: 'MIT'
+            }
+            , {
+                type: 'input',
+                name: 'website',
+                message: 'Your website:'
+            }], function (answers) {
+            this.appName = answers.name;
+            this.appDescription = answers.description;
+            this.appAuthor = answers.author;
+            this.appEmail = answers.email;
+            this.appRepo = answers.git;
+            this.appVersion = answers.version;
+            this.appLicense = answers.license;
+            this.appWebsite = answers.website;
+            done();
+        }.bind(this));
+    },
+    writing: function () {
+        this._createFileStructrure();
+    }
 });
--- app/templates/root/_gulpfile.js
+++ app/templates/root/_gulpfile.js
@@ -3,7 +3,28 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     coffee = require('gulp-coffee'),
     gutil = require('gulp-util'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    sass = require('gulp-sass'),
+    less = require('gulp-less'),
+    minifycss = require('gulp-minify-css'),
+    livereload = require('gulp-livereload'),
+    del = require('del'),
+    inject = require('gulp-inject'),
+    ngAnnotate = require('gulp-ng-annotate');
+
+var angular_order = [
+  'app/bower_components/**/angular.min.js',
+  'app/bower_components/**/angular-animate.min.js',
+  'app/bower_components/**/angular-aria.min.js',
+  'app/bower_components/**/angular-cookies.min.js',
+  'app/bower_components/**/angular-messages.min.js',
+  'app/bower_components/**/angular-resource.min.js',
+  'app/bower_components/**/angular-route.min.js',
+  'app/bower_components/**/angular-sanitize.min.js',
+  'app/bower_components/**/angular-touch.min.js',
+  'dist/*.min.js'
+];
 
 gulp.task('coffee', function() {
   gulp.src(['./app/**/*.coffee'])
@@ -15,6 +36,29 @@ gulp.task('concat', function() {
   return gulp.src(['./app/**/*.js'])
     .pipe(concat('main.js'))
     .pipe(rename({ suffix: '.min' }))
+    .pipe(ngAnnotate())
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./app/dist/js'));
+});
+
+gulp.task('sass', function () {
+gulp.src('./app/styles/*.scss')
+  .pipe(rename({ suffix: '.min'}))
+  .pipe(sass({outputStyle: 'compressed'}))
+  .pipe(gulp.dest('./app/dist/css'));
+});
+
+gulp.task('less', function () {
+  return gulp.src('./app/styles/**/*.less')
+    .pipe(less())
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./app/dist/css'));
+});
+
+gulp.task('inject', function () {
+  var target = gulp.src('index.html');
+  var sources = gulp.src(angular_order, {read:false}, {ignorePath: '/app'});
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest(''));
 });
