458a67522a963fea44858ed2657fbba9b98aad56
Thanos Korakas <thanoskorakas@gmail.com>
2015-12-13 22:44:33 +0200

    Gulp tasks.

--- app/index.js
+++ app/index.js
@@ -7,172 +7,168 @@ var chalk = require('chalk');
 
 module.exports = generators.Base.extend({
 
-    constructor: function () {
-        generators.Base.apply(this, arguments);
-        this.option('coffee');
-        this.option('less');
-        this.option('sass');
-        this.jsExtension = this.options.coffee ? "coffee" : "js";
-        if (!this.options.sass && !this.options.less) {
-            this.cssExtension = "css"
-        } else {
-            this.cssExtension = this.options.sass ? "scss" : "less"
-        }
-    },
-    _createFileStructrure: function () {
-        var destRoot = this.destinationRoot();
-        var appDir = destRoot + '/app';
-        var templateContext = {
-            appName: this.appName.split(" ").join(""),
-            appDescription: this.appDescription,
-            appVersion: this.appVersion,
-            appAuthor: this.appAuthor,
-            appEmail: this.appEmail,
-            appLicense: this.appLicense,
-            appGit: this.appGit,
-            appWebsite: this.appWebsite,
-            cssExtension: this.cssExtension,
-            coffee: this.jsExtension,
-            angularLib: this.angLibs,
-            angRouter: this.angRouter == 'angular-ui-router' ? 'ui.router' : 'ngRoute',
-            angInclude: this.angLibs.map(function(lib){
-              var library = lib.split('-');
-              var capitalize = library[1].charAt(0).toUpperCase() + library[1].slice(1);
-              return 'ng' + capitalize;
-            })
-        };
-        templateContext.angularLib.push(this.angRouter);
-
-        // Create folder structure.
-        mkdirp(appDir + '/js');
-        mkdirp(appDir + '/js/controllers');
-        mkdirp(appDir + '/js/services');
-        mkdirp(appDir + '/js/directives');
-        mkdirp(appDir + '/js/filters');
-        mkdirp(appDir + '/images');
-        mkdirp(appDir + '/tests');
-        mkdirp(appDir + '/templates');
-        this.fs.copy(this.sourceRoot() + '/root/_.bowerrc', destRoot + '/.bowerrc');
-        this.fs.copyTpl(this.sourceRoot() + '/root/_bower.json', destRoot + '/bower.json', templateContext);
-        this.fs.copyTpl(this.sourceRoot() + '/root/_index.html', appDir + '/index.html', templateContext);
-        this.fs.copyTpl(this.sourceRoot() + '/root/_app.' + this.jsExtension, appDir + '/app.' + this.jsExtension, templateContext);
-        this.fs.copyTpl(this.sourceRoot() + '/root/_gulpfile.js', destRoot + '/gulpfile.js', templateContext);
-        this.fs.copyTpl(this.sourceRoot() + '/root/_package.json', destRoot + '/package.json', templateContext);
-        this.fs.copyTpl(this.sourceRoot() + '/views/_main.html', appDir + '/templates/main.html', templateContext);
-        this.fs.copy(this.sourceRoot() + '/javascript/' + this.jsExtension + '/mainCtrl.' + this.jsExtension, appDir + '/js/controllers/mainCtrl.' + this.jsExtension);
-        if (this.option.less && this.option.sass) {
-            this.fs.copy(this.sourceRoot() + '/styles/style.' + this.cssExtension, appDir + '/styles/' + this.cssExtension + 'style.' + this.cssExtension);
-        } else {
-            this.fs.copy(this.sourceRoot() + '/styles/style.' + this.cssExtension, appDir + '/styles/style.' + this.cssExtension);
-        }
-    },
-    initializing: function () {
-        var message = chalk.blue('Welcome to Angularize ' + chalk.red('A simple AngularJS generator.'));
-        this.log(yosay(message, {maxLength: 21}));
-    },
-    prompting: function () {
-        var done = this.async();
-        this.prompt([{
-            type: 'input',
-            name: 'name',
-            message: 'Your project name:',
-            default: this.appname,
-        }
-            , {
-                type: 'input',
-                name: 'description',
-                message: 'Project description:'
-            }
-            , {
-                type: 'input',
-                name: 'author',
-                message: 'Your name:'
-            }
-            , {
-                type: 'input',
-                name: 'email',
-                message: 'Your email:'
-            }
-            , {
-                type: 'input',
-                name: 'git',
-                message: 'Your project git repository:'
-            }
-            , {
-                type: 'input',
-                name: 'version',
-                message: 'Project version:',
-                default: '0.0.0'
-            }
-            , {
-                type: 'input',
-                name: 'license',
-                message: 'Project license type:',
-                default: 'MIT'
-            }
-            , {
-                type: 'input',
-                name: 'website',
-                message: 'Your website:'
-            },{
-              type: 'checkbox',
-              name: 'libs',
-              message: 'Which angular libraries would you like to use?',
-              choices: [{
-                name: 'Angular aria',
-                value: 'angular-aria',
-                checked: true
-              },{
-                name: 'Angular animate',
-                value: 'angular-animate',
-                checked: false
-              },{
-                name: 'Angular messages',
-                value: 'angular-messages',
-                checked: true
-              },{
-                name: 'Angular resoure',
-                value: 'angular-resource',
-                checked: false
-              },{
-                name: 'Angular sanitize',
-                value: 'angular-sanitize',
-                checked: true
-              },{
-                name: 'Angular touch',
-                value: 'angular-touch',
-                checked: false
-              }]
-            },{
-              type: 'list',
-              name: 'router',
-              message: 'Which angular router would you like to use?',
-              choices: [{
-                name: 'ngRoute',
-                value: 'angular-route'
-              },{
-                name: 'UI Router',
-                value: 'angular-ui-router'
-              }], default: 1
-            }], function (answers) {
-            this.appName = answers.name;
-            this.appDescription = answers.description;
-            this.appAuthor = answers.author;
-            this.appEmail = answers.email;
-            this.appRepo = answers.git;
-            this.appVersion = answers.version;
-            this.appLicense = answers.license;
-            this.appWebsite = answers.website;
-            this.angLibs = answers.libs;
-            this.angRouter = answers.router;
-            done();
-        }.bind(this));
-    },
-    writing: function () {
-        this._createFileStructrure();
-    },
-    install: function () {
-        this.bowerInstall();
-        this.npmInstall();
+  constructor: function() {
+    generators.Base.apply(this, arguments);
+    this.option('coffee');
+    this.option('less');
+    this.option('sass');
+    this.jsExtension = this.options.coffee ? "coffee" : "js";
+    if (!this.options.sass && !this.options.less) {
+      this.cssExtension = "css"
+    } else {
+      this.cssExtension = this.options.sass ? "scss" : "less"
     }
+  },
+  _createFileStructrure: function() {
+    var destRoot = this.destinationRoot();
+    var appDir = destRoot + '/app';
+    var templateContext = {
+      appName: this.appName.split(" ").join(""),
+      appDescription: this.appDescription,
+      appVersion: this.appVersion,
+      appAuthor: this.appAuthor,
+      appEmail: this.appEmail,
+      appLicense: this.appLicense,
+      appGit: this.appGit,
+      appWebsite: this.appWebsite,
+      cssExtension: this.cssExtension,
+      coffee: this.jsExtension,
+      angularLib: this.angLibs,
+      angRouter: this.angRouter == 'angular-ui-router' ? 'ui.router' : 'ngRoute',
+      angInclude: this.angLibs.map(function(lib) {
+        var library = lib.split('-');
+        var capitalize = library[1].charAt(0).toUpperCase() + library[1].slice(1);
+        return 'ng' + capitalize;
+      })
+    };
+    templateContext.angularLib.push(this.angRouter);
+
+    // Create folder structure.
+    mkdirp(appDir + '/js');
+    mkdirp(appDir + '/js/controllers');
+    mkdirp(appDir + '/js/services');
+    mkdirp(appDir + '/js/directives');
+    mkdirp(appDir + '/js/filters');
+    mkdirp(appDir + '/images');
+    mkdirp(appDir + '/tests');
+    mkdirp(appDir + '/templates');
+    this.fs.copy(this.sourceRoot() + '/root/_.bowerrc', destRoot + '/.bowerrc');
+    this.fs.copyTpl(this.sourceRoot() + '/root/_bower.json', destRoot + '/bower.json', templateContext);
+    this.fs.copyTpl(this.sourceRoot() + '/root/_index.html', appDir + '/index.html', templateContext);
+    this.fs.copyTpl(this.sourceRoot() + '/root/_app.' + this.jsExtension, appDir + '/app.' + this.jsExtension, templateContext);
+    this.fs.copyTpl(this.sourceRoot() + '/root/_gulpfile.js', destRoot + '/gulpfile.js', templateContext);
+    this.fs.copyTpl(this.sourceRoot() + '/root/_package.json', destRoot + '/package.json', templateContext);
+    this.fs.copyTpl(this.sourceRoot() + '/views/_main.html', appDir + '/templates/main.html', templateContext);
+    this.fs.copy(this.sourceRoot() + '/javascript/' + this.jsExtension + '/mainCtrl.' + this.jsExtension, appDir + '/js/controllers/mainCtrl.' + this.jsExtension);
+    if (this.option.less && this.option.sass) {
+      this.fs.copy(this.sourceRoot() + '/styles/style.' + this.cssExtension, appDir + '/styles/' + this.cssExtension + 'style.' + this.cssExtension);
+    } else {
+      this.fs.copy(this.sourceRoot() + '/styles/style.' + this.cssExtension, appDir + '/styles/style.' + this.cssExtension);
+    }
+  },
+  initializing: function() {
+    var message = chalk.blue('Welcome to Angularize ' + chalk.red('A simple AngularJS generator.'));
+    this.log(yosay(message, {
+      maxLength: 21
+    }));
+  },
+  prompting: function() {
+    var done = this.async();
+    this.prompt([{
+      type: 'input',
+      name: 'name',
+      message: 'Your project name:',
+      default: this.appname,
+    }, {
+      type: 'input',
+      name: 'description',
+      message: 'Project description:'
+    }, {
+      type: 'input',
+      name: 'author',
+      message: 'Your name:'
+    }, {
+      type: 'input',
+      name: 'email',
+      message: 'Your email:'
+    }, {
+      type: 'input',
+      name: 'git',
+      message: 'Your project git repository:'
+    }, {
+      type: 'input',
+      name: 'version',
+      message: 'Project version:',
+      default: '0.0.0'
+    }, {
+      type: 'input',
+      name: 'license',
+      message: 'Project license type:',
+      default: 'MIT'
+    }, {
+      type: 'input',
+      name: 'website',
+      message: 'Your website:'
+    }, {
+      type: 'checkbox',
+      name: 'libs',
+      message: 'Which angular libraries would you like to use?',
+      choices: [{
+        name: 'Angular aria',
+        value: 'angular-aria',
+        checked: true
+      }, {
+        name: 'Angular animate',
+        value: 'angular-animate',
+        checked: false
+      }, {
+        name: 'Angular messages',
+        value: 'angular-messages',
+        checked: true
+      }, {
+        name: 'Angular resoure',
+        value: 'angular-resource',
+        checked: false
+      }, {
+        name: 'Angular sanitize',
+        value: 'angular-sanitize',
+        checked: true
+      }, {
+        name: 'Angular touch',
+        value: 'angular-touch',
+        checked: false
+      }]
+    }, {
+      type: 'list',
+      name: 'router',
+      message: 'Which angular router would you like to use?',
+      choices: [{
+        name: 'ngRoute',
+        value: 'angular-route'
+      }, {
+        name: 'UI Router',
+        value: 'angular-ui-router'
+      }],
+      default: 1
+    }], function(answers) {
+      this.appName = answers.name;
+      this.appDescription = answers.description;
+      this.appAuthor = answers.author;
+      this.appEmail = answers.email;
+      this.appRepo = answers.git;
+      this.appVersion = answers.version;
+      this.appLicense = answers.license;
+      this.appWebsite = answers.website;
+      this.angLibs = answers.libs;
+      this.angRouter = answers.router;
+      done();
+    }.bind(this));
+  },
+  writing: function() {
+    this._createFileStructrure();
+  },
+  install: function() {
+    this.bowerInstall();
+    this.npmInstall();
+  }
 });
--- app/templates/root/_gulpfile.js
+++ app/templates/root/_gulpfile.js
@@ -10,9 +10,8 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     del = require('del'),
     inject = require('gulp-inject'),
-    livereload = require('gulp-livereload'),
-    server = require('gulp-server-livereload'),
-    ngAnnotate = require('gulp-ng-annotate');
+    ngAnnotate = require('gulp-ng-annotate'),
+    gulpsync = require('gulp-sync')(gulp);
 
     var order = [
       './app/bower_components/**/angular.min.js',
@@ -31,22 +30,21 @@ var gulp = require('gulp'),
       'app/styles/*.css'
     ];
 <% if (coffee == 'coffee') { %>
-gulp.task('coffee', [], function() {
-  gulp.start('coffee_app', 'coffee_scripts');
-});
+gulp.task('coffee', gulpsync.sync(['coffee_app', 'coffee_scripts']));
+
 gulp.task('coffee_app', function() {
-  gulp.src('./app/app.coffee')
+  return gulp.src('./app/app.coffee')
     .pipe(coffee({bare: true}).on('error', gutil.log))
     .pipe(gulp.dest('./app/'))
 });
 gulp.task('coffee_scripts', function() {
-  gulp.src('./app/js/**/*.coffee')
+  return gulp.src('./app/js/**/*.coffee')
     .pipe(coffee({bare: true}).on('error', gutil.log))
     .pipe(gulp.dest('./app/js/'))
 });
 <% } %>
 
-gulp.task('scripts', function() {
+gulp.task('scripts_build', function() {
   return gulp.src(['./app/app.js', './app/js/**/*.js'])
     .pipe(concat('main.js'))
     .pipe(rename({ suffix: '.min' }))
@@ -56,24 +54,34 @@ gulp.task('scripts', function() {
 });
 <% if (cssExtension == 'scss') { %>
 // Sass
-gulp.task('styles', function () {
-  gulp.src('./app/styles/*.scss')
+gulp.task('styles_build', function () {
+  return gulp.src('./app/styles/*.scss')
   .pipe(rename({ suffix: '.min'}))
   .pipe(sass({outputStyle: 'compressed'}))
   .pipe(gulp.dest('./app/dist/css'));
+
+  gulp.task('styles', function () {
+    return gulp.src('./app/styles/*.scss')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(gulp.dest('./app/styles'));
 });
 <% } else if (cssExtension == 'less') { %>
   // Less
-gulp.task('styles', function () {
+gulp.task('styles_build', function () {
   return gulp.src('./app/styles/**/*.less')
     .pipe(less())
     .pipe(rename({ suffix: '.min'}))
     .pipe(minifycss())
     .pipe(gulp.dest('./app/dist/css'));
 });
+gulp.task('styles', function () {
+  return gulp.src('./app/styles/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./app/styles'));
+});
 <% } else { %>
 // css
-gulp.task('styles', function () {
+gulp.task('styles_build', function () {
   return gulp.src('./app/styles/**/*.less')
     .pipe(rename({ suffix: '.min'}))
     .pipe(minifycss())
@@ -81,37 +89,48 @@ gulp.task('styles', function () {
 });
 <% } %>
 
-gulp.task('inject', function () {
+gulp.task('index', function () {
  var target = gulp.src('./app/index.html');
  // var sources = gulp.src(order);
  return target.pipe(inject(gulp.src(order), {read:false, relative: true}))
    .pipe(gulp.dest('./app'));
 });
 
-// Default task
-gulp.task('default', ['clean'], function() {
-  gulp.start('styles', 'scripts', 'inject', 'images');
-});
-
-gulp.task('webserver', function() {
-  gulp.start('inject');
-  gulp.src('./app')
-    .pipe(server({
-      livereload: true,
-      directoryListing: false,
-      path: './app',
-      open: true
-    }));
-});
-// Watch
-gulp.task('watch', function() {
-  gulp.watch('./app/styles/**/*.less', ['less']);
-  gulp.watch('./app/**/*.js', ['scripts']);
-  livereload.listen();
-  gulp.watch(['./app/**']).on('change', livereload.changed);
-});
-
 // Clean
 gulp.task('clean', function() {
   return del('dist');
 });
+
+/////////////////////////////////////////////
+gulp.task('express', function() {
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')({port: 3000}));
+  app.use(express.static(__dirname + '/app'));
+  app.listen(8000, '0.0.0.0');
+});
+
+var tinylr;
+gulp.task('livereload', function() {
+  tinylr = require('tiny-lr')();
+    tinylr.listen(3000);
+});
+
+function notifyLiveReload(event) {
+  var fileName = require('path').relative(__dirname + '/app', event.path);
+  tinylr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
+gulp.task('watch', function() {
+  gulp.watch(['./app/styles/*.scss', './app/styles/*.less'], ['styles']);
+  gulp.watch('*.html', notifyLiveReload);
+  gulp.watch('./app/styles/*.css', notifyLiveReload);<% if (coffee == 'coffee') { %>
+  gulp.watch('./app/**/*.coffee', ['coffee']); <% } %>
+  gulp.watch('./app/**/*.js', notifyLiveReload);
+});
+
+gulp.task('default', gulpsync.sync([<% if (cssExtension != 'css') { %>'styles', <% } %> <% if (coffee == 'coffee') { %> 'coffee', <% } %>'index', 'express', 'livereload', 'watch']));
