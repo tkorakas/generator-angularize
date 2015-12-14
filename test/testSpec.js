ea2f0eaf17780ecf9e99dfad8b11aa94ed7708ba
Thanos Korakas <thanoskorakas@gmail.com>
2015-12-14 20:11:41 +0200

    Test failure with CI.

--- test/testSpec.js
+++ test/testSpec.js
@@ -4,6 +4,6 @@ var expect = require('chai').expect;
 
 describe('A basic test', function() {
   it('should be pass', function() {
-    expect(true).to.be.true;
+    expect(false).to.be.true;
   });
 });
